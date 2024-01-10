6971c4c563b2f0b815db0fc698446368383a4a7a
Lucas Silva <lucas.cristiano96@gmail.com>
2024-01-09 22:06:20 -0300

    Projeto criado

---
+++ src/main/java/br/com/alura/consulfipe/ConsulfipeApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.consulfipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsulfipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsulfipeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/alura/consulfipe/ConsulfipeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.consulfipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsulfipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
